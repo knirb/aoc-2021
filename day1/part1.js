2594f31442ef956994332e177f6825a3c4833a42
knirb <viktorasbrink@gmail.com>
2021-12-02 20:15:20 +0100

    chaining is fun

--- day1/part1.js
+++ day1/part1.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 fs.readFile("day1/input.txt", "utf8", (err, data) => {
-  const splitData = data.split("\n").map((depth) => parseInt(depth));
-  const sum = splitData.reduce((acc, curVal, index, arr) => {
-    acc += curVal > arr[index - 1] ? 1 : 0;
-    return acc;
-  }, 0);
+  const sum = data
+    .split("\n")
+    .map((depth) => parseInt(depth))
+    .reduce((acc, curVal, index, arr) => {
+      acc += curVal > arr[index - 1] ? 1 : 0;
+      return acc;
+    }, 0);
   console.log(sum);
 });
