be318ea01f71ad4465e023be6975ec1ef68edf95
knirb <viktorasbrink@gmail.com>
2021-12-02 20:25:57 +0100

    solve part2 (with more chaining :D)

---
+++ day1/part2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+fs.readFile("day1/input.txt", "utf8", (err, data) => {
+  const sum = data
+    .split("\n")
+    .map((depth) => parseInt(depth))
+    .reduce((acc, depth, index, arr) => {
+      if (index < 1 || index === arr.length - 1) return acc;
+      acc.push(arr[index - 1] + arr[index] + arr[index + 1]);
+      return acc;
+    }, [])
+    .reduce((acc, curVal, index, arr) => {
+      console.log(curVal);
+      acc += curVal > arr[index - 1] ? 1 : 0;
+      return acc;
+    }, 0);
+
+  console.log(sum);
+});
