15a1b39df2e393d3a648a1aea300388200086d32
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-12-03 19:27:24 +0100

    solve day3/prob2, no chains, sad

---
+++ day3/prob2.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+fs.readFile("./input.txt", "utf8", (err, data) => {
+  const getAns = (data) => {
+    const rec = (arr, ind, more) => {
+      if (arr.length === 1) return parseInt(arr[0].join(""), 2);
+      const filt =
+        arr.reduce((acc, cur) => {
+          acc += cur[ind] ? 1 : -1;
+          return acc;
+        }, 0) >= 0
+          ? more
+            ? 1
+            : 0
+          : more
+          ? 0
+          : 1;
+      return rec(
+        arr.filter((nr) => nr[ind] === filt),
+        ind + 1,
+        more
+      );
+    };
+    return rec(data, 0, true) * rec(data, 0, false);
+  };
+  const arr = data.split("\n").map((string) => string.split("").map((str) => parseInt(str)));
+
+  console.log(getAns(arr));
+});
