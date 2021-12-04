2eeadff8a9473d126f4117c4f941dbdb7b3b7ee2
knirb <viktorasbrink@gmail.com>
2021-12-04 12:45:24 +0100

    tmp commit, moving pc

---
+++ day4/prob1.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+fs.readFile("day4/input.txt", "utf8", (err, data) => {
+  const [list, ...tables] = data
+    .split("\n\n")
+    .map((row) => row.split("\n"))
+    .map((table) =>
+      table.length > 1 ? table.map((row) => row.split(" ").filter((cell) => cell)) : table[0].split(",")
+    );
+
+  const [szy, szx] = [tables[0].length, tables[0][0].length];
+
+  const bingo = (table) => {
+    console.log(table);
+    for (let i = 0; i < table.length; i++) {
+      if (table[i].reduce((acc, cur) => acc + cur, 0) >= table.length) {
+        return true;
+      }
+      let sum = 0;
+      for (let y = 0; y < table.length; y++) {
+        sum += table[y][i];
+      }
+      if (sum >= table.length) {
+        return true;
+      }
+    }
+    // let d1 = 0,
+    //   d2 = 0;
+    // for (let i = 0; i < table.length; i++) {
+    //   d1 += table[i][i];
+    //   d2 += table[i][table.length - 1 - i];
+    // }
+    // if (d1 >= table.length || d2 >= table.length) {
+    //   return true;
+    // }
+    return false;
+  };
+
+  const findIndex = (table, nr) => {
+    for (let y = 0; y < table.length; y++) {
+      const x = table[y].findIndex((cell) => cell === nr);
+      if (x > 0) return [x, y];
+    }
+    return null;
+  };
+
+  const calcSum = (t1, t2) => {
+    let sum = 0;
+    for (let y = 0; y < t1.length; y++) {
+      for (let x = 0; x < t1[y].length; x++) {
+        sum += !t1[y][x] ? parseInt(t2[y][x]) : 0;
+      }
+    }
+    return sum;
+  };
+
+  const bingoArr = list.reduce((acc, cur, ind, arr) => {
+    if (acc.length < 4) return acc;
+    if (ind === 0) {
+      for (let len = 0; len < acc.length; len++) {
+        acc[len] = [];
+        for (let y = 0; y < szy; y++) {
+          acc[len][y] = [];
+          for (let x = 0; x < szx; x++) {
+            acc[len][y][x] = 0;
+          }
+        }
+      }
+    }
+    tables.forEach((table, i) => {
+      const coords = findIndex(table, cur);
+      if (coords) {
+        acc[i][coords[1]][coords[0]] = 1;
+      }
+    });
+    if (ind > 20) {
+      const id = acc.findIndex((table) => bingo(table));
+      if (id >= 0) {
+        // console.log("bingo!", id, ind, parseInt(cur));
+      }
+      if (id >= 0) return [acc[id], tables[id], parseInt(cur)];
+    }
+    return acc;
+  }, new Array(tables.length));
+
+  const ans = calcSum(...bingoArr.slice(0, 2)) * bingoArr[2];
+  console.log(calcSum(...bingoArr.slice(0, 2)), bingoArr[0], bingoArr[1], bingoArr[2]);
+  console.log(ans);
+});
