7323714b01289269f5ff10b466f3ab6588364add
knirb <viktorasbrink@gmail.com>
2021-12-07 22:02:45 +0100

    separate prob2 to own fiel

--- day7/prob2.js
+++ day7/prob2.js
@@ -8,7 +8,10 @@ const data = fs
 const maxDist = Math.max(...data);
 let maxFuel = Number.POSITIVE_INFINITY;
 for (let i = 0; i < maxDist + 1; i++) {
-  const cost = data.reduce((acc, cur) => acc + Math.abs(cur - i), 0);
+  const cost = data.reduce((acc, cur) => {
+    const n = Math.abs(cur - i);
+    return acc + (n * (n + 1)) / 2;
+  }, 0);
   if (cost < maxFuel) maxFuel = cost;
 }
 
