c8a770921ba12258618ef2167a5e78e0e98585b2
Viktor Åsbrink <viktor@kreationsbyran.se>
2021-12-23 15:44:22 +0100

    formatting

--- day4/prob2.js
+++ day4/prob2.js
@@ -1,5 +1,43 @@
 const fs = require("fs");
 
+const bingo = (table) => {
+  for (let i = 0; i < table.length; i++) {
+    if (
+      table[i].reduce((acc, cur) => {
+        return (acc += cur === "X" ? 1 : 0);
+      }, 0) >= table.length
+    ) {
+      return true;
+    }
+    let sum = 0;
+    for (let y = 0; y < table.length; y++) {
+      sum += table[y][i] === "X" ? 1 : 0;
+    }
+    if (sum >= table.length) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const findIndex = (table, nr) => {
+  for (let y = 0; y < table.length; y++) {
+    const x = table[y].findIndex((cell) => cell === nr);
+    if (x >= 0) return [x, y];
+  }
+  return null;
+};
+
+const calcSum = (t1) => {
+  let sum = 0;
+  for (let y = 0; y < t1.length; y++) {
+    for (let x = 0; x < t1[y].length; x++) {
+      sum += t1[y][x] !== "X" ? parseInt(t1[y][x]) : 0;
+    }
+  }
+  return sum;
+};
+
 fs.readFile("day4/input.txt", "utf8", (err, data) => {
   const [list, ...tables] = data
     .split("\n\n")
@@ -9,44 +47,6 @@ fs.readFile("day4/input.txt", "utf8", (err, data) => {
         ? table.map((row) => row.split(" ").filter((cell) => cell))
         : table[0].split(",")
     );
-
-  const bingo = (table) => {
-    for (let i = 0; i < table.length; i++) {
-      if (
-        table[i].reduce((acc, cur) => {
-          return (acc += cur === "X" ? 1 : 0);
-        }, 0) >= table.length
-      ) {
-        return true;
-      }
-      let sum = 0;
-      for (let y = 0; y < table.length; y++) {
-        sum += table[y][i] === "X" ? 1 : 0;
-      }
-      if (sum >= table.length) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  const findIndex = (table, nr) => {
-    for (let y = 0; y < table.length; y++) {
-      const x = table[y].findIndex((cell) => cell === nr);
-      if (x >= 0) return [x, y];
-    }
-    return null;
-  };
-
-  const calcSum = (t1) => {
-    let sum = 0;
-    for (let y = 0; y < t1.length; y++) {
-      for (let x = 0; x < t1[y].length; x++) {
-        sum += t1[y][x] !== "X" ? parseInt(t1[y][x]) : 0;
-      }
-    }
-    return sum;
-  };
 
   const winner = list.reduce(
     (acc, cur, ind, arr) => {
